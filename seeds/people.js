8a65f7e1b785b8f79bd75717d17ab8fa4c174b81
Abhii-now <19ucs240@lnmiit.ac.in>
2021-09-14 22:28:02 +0530

    created banking system, added seeds, added transaction functionality and more

---
+++ seeds/people.js
@@ -0,0 +1,52 @@
+module.exports = [
+  {
+    gender: "male",
+    name: { title: "Mr", first:	 "Jim", last: "Lee" },
+    email: "jim.lee@example.com",
+  },
+  {
+    gender: "male",
+    name: { title: "Mr", first: "Barry", last: "Moreno" },
+    email: "barry.moreno@example.com",
+  },
+  {
+    gender: "female",
+    name: { title: "Miss", first: "Brigitta", last: "Böhler" },
+    email: "brigitta.bohler@example.com",
+  },
+  {
+    gender: "female",
+    name: { title: "Ms", first: "Fraukje", last: "Saman" },
+    email: "fraukje.saman@example.com",
+  },
+  {
+    gender: "female",
+    name: { title: "Mrs", first: "Mylène", last: "Mijnals" },
+    email: "mylene.mijnals@example.com",
+  },
+  {
+    gender: "male",
+    name: { title: "Mr", first: "Austin", last: "Kumar" },
+    email: "austin.kumar@example.com",
+  },
+  {
+    gender: "male",
+    name: { title: "Mr", first: "Eeli", last: "Walli" },
+    email: "eeli.walli@example.com",
+  },
+  {
+    gender: "female",
+    name: { title: "Miss", first: "Vivan", last: "Patterson" },
+    email: "vivan.patterson@example.com",
+  },
+  {
+    gender: "female",
+    name: { title: "Ms", first: "Amy", last: "Caldwell" },
+    email: "amy.caldwell@example.com",
+  },
+  {
+    gender: "male",
+    name: { title: "Mr", first: "Matias", last: "Valli" },
+    email: "matias.valli@example.com",
+  },
+];
